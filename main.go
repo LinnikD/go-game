c76b4bc5436b1bf7e77017fe651b0a0a9ad46577
Denis Linnik <d.linnik@corp.mail.ru>
2017-08-14 21:44:14 +0300

    bot initial

--- main.go
+++ main.go
@@ -1,6 +1,27 @@
-package goGame
+package main
+
+import (
+	tgbotapi "gopkg.in/telegram-bot-api.v4"
+)
 
 func main () {
+	bot, err := tgbotapi.NewBotAPI("385402864:AAEmuWbihbSEVV7-8Jy0CDLSeLMcrPpI86s")
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 1
+
+	updates, _ := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		} else {
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Hi")
+			msg.ReplyToMessageID = update.Message.MessageID
+
+			bot.Send(msg)
+		}
+	}
 
 }
 
